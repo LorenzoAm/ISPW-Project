0d28528f98ca98647970d1baddcc2ab7b87e4720
Andrei <Andrei@DESKTOP-F0TAT6O.homenet.telecomitalia.it>
2021-01-03 15:06:53 +0100

    Implementato funzionalità RadioButton in SignUp

--- SkateSpot/src/logic/gui/SignUpGuiController.java
+++ SkateSpot/src/logic/gui/SignUpGuiController.java
@@ -1,11 +1,15 @@
 package logic.gui;
 
-//import javax.swing.JRadioButton;
+import javax.swing.JOptionPane;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import logic.bean.SignUpBean;
 
@@ -13,17 +17,38 @@ import logic.bean.SignUpBean;
 public class SignUpGuiController
 {
 	//fxml
-	public TextField name;
-	public TextField surname;
-	public TextField email;
-	public PasswordField password;
-	public PasswordField confirmPassword;
-	public DatePicker data;
-	//private JRadioButton genderMale;
-	//private JRadioButton genderFemale;
+	@FXML public TextField name;
+	@FXML public TextField surname;
+	@FXML public TextField email;
+	@FXML public PasswordField password;
+	@FXML public PasswordField confirmPassword;
+	@FXML public DatePicker data;
+	@FXML private RadioButton maleButton;
+	@FXML private RadioButton femaleButton;
+	private String gender;
+	ToggleGroup radioToggleGroup = new ToggleGroup();
 
+	@FXML
+	private void radioButtonChanged(ActionEvent event)
+	{
+		if(maleButton.isSelected())
+		{
+			gender="Male";
+		}
+		if(femaleButton.isSelected())
+		{
+			gender="Female";
+		}
+	}
+	@FXML
+	public void initialize()
+	{
+		maleButton.setToggleGroup(radioToggleGroup);
+		femaleButton.setToggleGroup(radioToggleGroup); 
+	}
     public void handle(MouseEvent mouseEvent)
     {
+    	this.initialize();
         Button button = (Button) mouseEvent.getSource();
         String value = button.getText();
         switch(value)
@@ -33,10 +58,9 @@ public class SignUpGuiController
                 HomeMain.getStage().show();
                 break;
             case "Sign up":     //sign up button clicked --> create bean class
-            	 SignUpBean bean = new SignUpBean(name.getText(),surname.getText(),email.getText(),password.getText(),confirmPassword.getText(), data.getValue());
-            	 System.out.println(bean.toString());
+            	SignUpBean bean = new SignUpBean(name.getText(),surname.getText(),email.getText(),password.getText(),confirmPassword.getText(), data.getValue(),gender);
                 break;
         }
 
-    }
+    }	
 }
